72eb20f840091919db96a342e5fff0e51a41fa8c
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-17 09:40:13 -0600

    Created ChatBot, and edited getignore.

---
+++ src/chat/controller/ChatController.java
@@ -0,0 +1,19 @@
+package chat.controller;
+
+public class ChatController 
+{
+//	private PopupDisplay display;
+	
+	public ChatController()
+	{
+		
+	}
+	
+	public void start()
+	{
+		
+		
+		
+		
+	}
+}
