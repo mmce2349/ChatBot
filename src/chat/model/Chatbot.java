13c7828d9687a839e157a682554f29add1e85503
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-10-25 09:51:31 -0600

    Added shopping list items, and verbs list.

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -20,8 +20,8 @@ public class Chatbot
 	
 	public Chatbot(String username)
 	{
-		this.movieList = null;
-		this.shoppingList = null;
+		this.movieList = new ArrayList<Movie>();
+		this.shoppingList = new ArrayList<String>();
 		this.cuteAnimalMemes = null;
 		this.currentTime = null;
 		this.questions = null;
@@ -30,17 +30,36 @@ public class Chatbot
 		this.intro = null;
 		this.currentTime = null;
 		this.topics = null;
-		this.verbs = null;
+		this.verbs = new String [4];
 		this.followUps = null;
+		
+		buildVerbs();
+		buildShoppingList();
+	}
+	
+	private void buildVerbs()
+	{
+		verbs[0] = "like";
+		verbs[1] = "dislike";
+		verbs[2] = "ambivalent about";
+		verbs[3] = "am thinking about";
 	}
 
 	private void buildMovieList()
 	{
-		
+//		movieList.add("");
+//		movieList.add("Spiderman");
+//		movieList.add("Hidden Figure");
+//		
 	}
 	
 	private void buildShoppingList()
 	{
+		shoppingList.add("snacks");
+		shoppingList.add("veggies");
+		shoppingList.add("protein");
+		shoppingList.add("fruits");
+		shoppingList.add("grains");
 		
 	}
 	
@@ -61,7 +80,13 @@ public class Chatbot
 	
 	public boolean lengthChecker(String input)
 	{
-		return false;
+		boolean validLength = false;
+		
+		if (input != null && input.length() > 2)
+		{
+				validLength = true;
+		}
+		return validLength;
 	}
 	
 	public boolean htmlTagChecker(String input)
