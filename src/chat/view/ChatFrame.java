0b683a05e4173c972c972c019b1d1fa03d4638a8
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-11-15 08:42:07 -0700

    Added more code to chatframe. Such as setting size, and title for frame.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -7,20 +7,22 @@ import javax.swing.JFrame;
 public class ChatFrame extends JFrame
 {
 	private ChatbotController appController;
-	
+//	private ChatPanel appPanel;
 	
 	public ChatFrame(ChatbotController appController)
 	{
 		super();
 		this.appController = appController;
-		
+	//	appPanel = new ChatPanel(appController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		
-		
+		this.setSize(500, 500);
+		this.setTitle("Chatbot 2017");
+//		this.setContentPane(appPanel);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 }
