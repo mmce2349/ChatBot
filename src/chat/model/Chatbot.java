d74e939e8a9a3bff6cb959f459fe26bf76a9501d
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2017-11-28 07:56:18 -0700

    We added a switch case for our followups

--- src/chat/model/Chatbot.java
+++ src/chat/model/Chatbot.java
@@ -179,6 +179,25 @@ public class Chatbot
 			random = (int) (Math.random() * movieList.size());
 			response += "\n" + movieList.get(random).getTitle() + " is a great movie!";
 		}
+		//added november 28 
+		int followup = (int) (Math.random() * 5);
+				switch(followup)
+				{
+				case 0:
+					response += followUps[0] + "\n";
+					break;
+				case 3:
+					response += followUps[1] + "\n";
+				case 1:
+					response += followUps[2] + "\n";
+					break;
+				default:
+					response += followUps[4] + "\n";
+					response += followUps[3] + "\n";
+					break;
+					
+				
+				}
 		return response; 
 	}
 	/**
