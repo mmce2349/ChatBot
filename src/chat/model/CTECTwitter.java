3252b1ac9b262fa46b593c45a339200ba9b4280c
mmce2349 <31513795+mmce2349@users.noreply.github.com>
2018-03-06 09:01:24 -0700

    cool

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -58,7 +58,7 @@ public class CTECTwitter
 		turnStatusesToWords();
 		totalWordCount = tweetedWords.size();
 		String [] boring = createIgnoredWordArray();
-		
+		trimTheBoringWords(boring);
 		
 		return mostCommon;
 		
